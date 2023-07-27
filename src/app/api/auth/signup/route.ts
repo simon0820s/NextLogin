247af5afa000c263e03b3d8aa710cd6115ea9688
simon0820s <simon0820s@gmail.com>
2023-07-27 17:28:25 -0500

    get data

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,6 +1,15 @@
 import { NextResponse } from "next/server"
 
 export async function POST(request: Request) {
-    const {fullName, email, passoword} = await request.json()
+
+    const {fullname, email, password} = await request.json()
+    console.log(fullname,email,password)
+
+    if (!password || password.length < 6) return NextResponse.json({
+        message: "Password must be at least 6 characters"
+    },{
+        status: 400
+    })
+
     return NextResponse.json({message: "signUp"})
 }
