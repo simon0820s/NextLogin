16d265f1f5b81d1b36c8945f8e40d69382e2449e
simon0820s <simon0820s@gmail.com>
2023-07-27 17:36:40 -0500

    create register api

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,15 +1,36 @@
 import { NextResponse } from "next/server"
+import User from '@/models/user'
+import bcrypt from 'bcryptjs'
 
 export async function POST(request: Request) {
 
-    const {fullname, email, password} = await request.json()
-    console.log(fullname,email,password)
+    const { fullname, email, password } = await request.json()
+    console.log(fullname, email, password)
 
     if (!password || password.length < 6) return NextResponse.json({
         message: "Password must be at least 6 characters"
-    },{
+    }, {
         status: 400
     })
 
-    return NextResponse.json({message: "signUp"})
+    const userFound = await User.findOne({ email });
+
+    if (userFound) return NextResponse.json({
+        message: "Email already exists"
+    }, {
+        status: 409
+    });
+
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    const user = new User({
+        email,
+        fullname,
+        password: hashedPassword
+    })
+
+    const savedUser = await user.save()
+    console.log(savedUser)
+
+    return NextResponse.json(savedUser)
 }
