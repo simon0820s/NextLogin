572eddd5bf0f108aaedb6c88646a0f39cd7f2a99
simon0820s <simon0820s@gmail.com>
2023-07-28 22:44:17 -0500

    functional register form ready

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,60 @@
-import React from 'react'
+"use client"
+
+import axios, { AxiosError } from 'axios'
+import { FormEvent, useState } from 'react'
 
 export default function RegisterPage() {
+
+  const [error, setError] = useState();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+
+    try {
+      const response = await axios.post('/api/auth/signup', {
+        email: formData.get('email'),
+        password: formData.get('password'),
+        fullname: formData.get('fullname')
+      })
+      console.log(response)
+      setError(undefined)
+    } catch (error) {
+      if (error instanceof AxiosError){
+        setError(error.response?.data.message)
+      }
+    }
+  }
+
   return (
-    <div>RegisterPage</div>
+    <div className='p-8'>
+      <form className='flex flex-col gap-2' onSubmit={handleSubmit} >
+        <h1 className='text-neutral-100 font-bold text-2xl'>Signup</h1>
+        <input
+          type='text'
+          placeholder='Simon Arboleda'
+          name='fullname'
+          className='bg-zinc-800 px-4 py-2 block mb-2 rounded-sm' />
+
+        <input
+          type='email'
+          placeholder='simon0820s@gmail.com'
+          name='email'
+          className='bg-zinc-800 px-4 py-2 block mb-2 rounded-sm' />
+
+        <input
+          type='password'
+          placeholder='*******'
+          name='password'
+          className='bg-zinc-800 px-4 py-2 block mb-2 rounded-sm' />
+        
+        {
+          error && <p className='text-xs text-red-600 font-bold'> { error } </p>
+        }
+        <button type='submit' className='bg-indigo-500 px-4 py-2 rounded-md font-semibold hover:bg-indigo-700 transition-all ease-in-out duration-400  '>
+          Signup
+        </button>
+      </form>
+    </div>
   )
 }
