5489c56c25aeb5251a62f7d91b0e7c0358a58afd
simon0820s <simon0820s@gmail.com>
2023-07-29 11:44:19 -0500

    create basic signin

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: 'credentials',
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "jsmith@gmail.com"},
+                password: { label: "Password", type: "password", placeholder: "*******"}
+            },
+            authorize(credentials, req) {
+                const user = { id: "1", fullname: 'J Smith', email: 'sar@gmail.com' }
+                return user
+            }
+        })
+    ]
+})
+
+export { handler as GET, handler as POST}
