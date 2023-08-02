9d4e296a9ccc6b170bb15d351663db163047cfa4
simon0820s <simon0820s@gmail.com>
2023-08-02 00:19:40 -0500

    condicional navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import Link from 'next/link'
-import React from 'react'
+import { getServerSession } from 'next-auth'
 
-export default function Navbar() {
+export default async function Navbar() {
+
+    const session = await getServerSession()
+    console.log(session)
+
     return (
         <nav className='bg-zinc-900 p-4'>
             <div className='flex justify-between container mx-auto'>
@@ -9,19 +13,27 @@ export default function Navbar() {
                     <h1 className='font-bold text-2xl text-indigo-400'>NextAuth</h1>
                 </Link>
                 <ul className='flex gap-x-2'>
-                    <li className='px-3 py-1'>
-                        <Link className='hover:text-violet-400 transition-all ease-in-out duration-500' href="/dashboard/profile">Profile</Link>
-                    </li>
-                    <li className='px-3 py-1'>
-                        <Link className='hover:text-violet-400 transition-all ease-in-out duration-500' href="/login">Login</Link>
-                    </li>
-                    <li className='px-3 py-1'>
-                        <Link className='hover:text-violet-400 transition-all ease-in-out duration-500' href="/register">Register</Link>
-                    </li>
+                    {
+                        session ? (
+                            <li className='px-3 py-1'>
+                                <Link className='hover:text-violet-400 transition-all ease-in-out duration-500' href="/dashboard/profile">Profile</Link>
+                            </li>
+                        ) : (
+                            <>
+                                <li className='px-3 py-1'>
+                                    <Link className='hover:text-violet-400 transition-all ease-in-out duration-500' href="/login">Login</Link>
+                                </li>
+                                <li className='px-3 py-1'>
+                                    <Link className='hover:text-violet-400 transition-all ease-in-out duration-500' href="/register">Register</Link>
+                                </li>
 
-                    <li className='px-3 py-1'>
-                        <Link className='hover:text-violet-400 transition-all ease-in-out duration-500' href="/about">About</Link>
-                    </li>
+                                <li className='px-3 py-1'>
+                                    <Link className='hover:text-violet-400 transition-all ease-in-out duration-500' href="/about">About</Link>
+                                </li>
+                            </>
+                        )
+                    }
+
                 </ul>
             </div>
         </nav>
